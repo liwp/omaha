d9247cc4a3a8561617f238d3b67c6ff0168f3c3e
Lauri Pesonen <lauri.pesonen@iki.fi>
2013-04-12 12:06:01 -0700

    fix utils tests

--- base/utils_unittest.cc
+++ base/utils_unittest.cc
@@ -556,7 +556,7 @@ TEST(UtilsTest, AddAllowedAce) {
 
   EXPECT_SUCCEEDED(File::Copy(
       ConcatenatePath(app_util::GetCurrentModuleDirectory(),
-                      _T("GoogleUpdate.exe")),
+                      _T("NinjaDeployUpdate.exe")),
       test_file_path,
       false));
 
